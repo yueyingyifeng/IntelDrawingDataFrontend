9cbec71bbe568eaa7707f0eae4a29e08f334bf16
yueyingyifeng <yueyingyifeng@Gmail.com>
2024-09-19 23:47:19 +0800

    完善了创建表格

--- src/components/HomeComponents/Home_CreateTableComponent.vue
+++ src/components/HomeComponents/Home_CreateTableComponent.vue
@@ -1,12 +1,40 @@
 <script setup>
-import { ref, watch } from 'vue';
+import { ref, watch, defineEmits, defineProps } from 'vue';
 import { ElMessage } from 'element-plus'
+import axios from 'axios';
 import ArowConponent from '../CreateTableComponents/ArowConponent.vue';
-
+import {store} from '@/Util/Store.js'
 const tableData = ref([[]]);
 const count_add = ref(1)
 const saveDialog = ref(false)
 const file_name = ref('')
+const blank_character = ref(0)
+const ChartTypes = [
+  { value: 'bar', label: 'Bar Chart' },
+  { value: 'line', label: 'Line Chart' },
+  { value: 'pie', label: 'Pie Chart' },
+  { value: 'radar', label: 'Radar Chart' },
+  { value: 'polar', label: 'Polar Chart' },
+  { value: 'doughnut', label: 'Doughnut Chart' },
+  { value: 'bubble', label: 'Bubble Chart' },
+]
+const selectedType = ref(ChartTypes[0].value)
+
+
+const emit = defineEmits(['saveFile', 'previewData']);
+
+
+const props = defineProps({
+  show:{
+        type: Boolean,
+        default: true
+  }
+})
+
+watch(blank_character, (newVal, oldVal) => {
+  if(newVal == '')
+    blank_character.value = '0'
+})
 
 watch(count_add, (newVal, oldVal) => {
   if (newVal > 10)
@@ -19,6 +47,8 @@ watch(count_add, (newVal, oldVal) => {
 //   if(oldVal)
 //     ElMessage.warning("File did not save")
 // })
+
+
 
 function addRow() {
   for (let i = 0; i < count_add.value; i++)
@@ -34,19 +64,58 @@ function deleteRow() {
 }
 
 function showSaveFileDialog() {
+  if(tableData.value.length > 1 && tableData.value[0].length <= 0){
+    ElMessage.warning("Please add at least one head col")
+    return
+  }
+
+  if(tableData.value[0].length <= 0){{
+    ElMessage.warning("Please add at least one col")
+    return
+  }}
+    
+
   saveDialog.value = true;
 }
 
+function previewData() {
+  emit('previewData', tableData.value);
+}
+
 function saveFile() {
-  console.log(tableData.value)
+  
 
   if(!isValidWindowsFilename(file_name.value)){
     ElMessage.error("Invalid file name, did you add a space in the end?")
     return
   }
-  saveDialog.value = false;  
-}
+  
 
+  tableData.value = fillArray(tableData.value, blank_character.value)
+
+  console.log(tableData.value)
+  axios({
+    method: 'post',
+    url: 'https://localhost:7161/api/CreateTable',
+    headers: {
+        'Authorization': `Bearer ${store.getUserData().token}`
+    },
+    data: {
+        'fileName': file_name.value,
+        'data': tableData.value
+    }
+  }).then(res => {
+    if(res.status === 201)
+      ElMessage.success("File saved successfully")
+    console.log(res)
+  }).catch(err => {
+    console.log(err)
+  })
+
+  emit('saveFile');
+  saveDialog.value = false;
+}
+// TODO: 记得给注册用户名也加上这样的判断，呃。。。移到另一个文件吧
 function isValidWindowsFilename(filename) {
     // 1. 检查是否包含非法字符
     const invalidChars = /[<>:"\/\\|?*]/;
@@ -82,10 +151,32 @@ function isValidWindowsFilename(filename) {
     return true;
 }
 
+function fillArray(arr, fillValue) {
+    fillValue = String(fillValue);
+    // 找到二维数组中最长的数组长度
+    const maxLength = Math.max(...arr.map(subArray => subArray.length));
+
+    // 遍历每个子数组并用 fillValue 填充使其长度达到 maxLength
+    return arr.map(subArray => {
+        // 如果子数组长度小于 maxLength，则用 fillValue 填充
+        return [...subArray, ...Array(maxLength - subArray.length).fill(fillValue)];
+    });
+}
 </script>
 
 <template>
-  <main>
+  <main v-show="show">
+    <p>
+      {{ tableData.length }} X {{ tableData[0].length }}
+    </p>
+    <p>
+      空白单元用
+      <el-input v-model="blank_character" style="width: 60px;" type="text" />
+      代替
+    </p>
+
+    <hr/>
+
     <ArowConponent v-for="(item, index) in tableData" :row="tableData[index]" />
 
     <el-row>
@@ -105,12 +196,23 @@ function isValidWindowsFilename(filename) {
       <el-col :span="4"></el-col>
 
       <el-col :span="4">
+        <el-button type="primary" @click="previewData">
+          Preview Data
+        </el-button>
         <el-button type="success" @click="showSaveFileDialog  ">Save</el-button>
+        <el-select v-model="selectedType">
+          <el-option v-for="item in ChartTypes" 
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"/>
+        </el-select>
       </el-col>
+      
     </el-row>
 
   </main>
 
+<!-- 确认信息框 -->
   <el-dialog v-model="saveDialog" title="Save Data" width="500" draggable overflow>
     <label>File Name: </label>
     <el-input v-model="file_name" style="width: auto;" type="text" placeholder="Enter the name of the file" />
@@ -123,7 +225,7 @@ function isValidWindowsFilename(filename) {
         </el-button>
       </div>
     </template> 
-  </el-dialog>
+  </el-dialog><!-- el-dialog -->
 </template>
 
 <style></style>
